77de36960f5d56d3bbc67369d25a06dfebd90452
Yuri Duarte <yuriduartedev@gmail.com>
2019-08-05 10:11:38 -0300

    Retira a obrigatoriedade de alguns campos dos CRUDS

--- lib/aquario/species/specy.ex
+++ lib/aquario/species/specy.ex
@@ -36,6 +36,6 @@ defmodule Aquario.Species.Specy do
     |> cast(attrs, [:name_pt_br, :name_en_us, :name_es_es, :scientific_name_pt_br, :scientific_name_en_us, :scientific_name_es_es, :order, :image, :description_pt_br, :description_en_us, :description_es_es, :title_pt_br, :title_en_us, :title_es_es])
     |> put_change(:image_token, Ecto.UUID.generate())
     |> cast_attachments(attrs, [:image])
-    |> validate_required([:name_pt_br, :name_en_us, :name_es_es, :scientific_name_pt_br, :scientific_name_en_us, :scientific_name_es_es, :order, :image, :description_pt_br, :description_en_us, :description_es_es, :title_pt_br, :title_en_us, :title_es_es])
+    |> validate_required([:name_pt_br, :scientific_name_pt_br, :description_pt_br ])
   end
 end
